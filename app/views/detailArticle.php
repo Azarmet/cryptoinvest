fc79c14c5b46d9d58525533436732030db385958
Roirand Lucas <roirand.lucas@outlook.fr>
2025-03-28 18:58:09 +0100

    modification detail article photo

--- app/views/detailArticle.php
+++ app/views/detailArticle.php
@@ -2,6 +2,7 @@
 
 
 <h1><?= htmlspecialchars($article['titre']) ?></h1>
+<img src="<?= RACINE_URL ."/public/uploads/article/" . $article['image']?>" alt="">
     <p><em><?= htmlspecialchars($article['categorie']) ?> - <?= $article['date_publication'] ?></em></p>
     <div><?= nl2br($article['contenu']) ?></div>
     <p><a href="index.php?page=learn">← Retour</a></p>
