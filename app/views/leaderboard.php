3f8763c8bfd2a0b2d861d5b3e39ef83261b0a084
Roirand Lucas <roirand.lucas@outlook.fr>
2025-04-01 08:59:19 +0200

    modif leaderboard ligne cliquable

--- app/views/leaderboard.php
+++ app/views/leaderboard.php
@@ -3,18 +3,22 @@
 <div class="leaderboard-container">
     <h1>Leaderboard</h1>
     <?php $tableRows = ''; ?>
-    <?php foreach ($usersWithSolde as $user): 
-        $photoProfil = "<img src=\"{$user['image']}\" alt=\"Profil\" width=\"25\">";
-        $pseudo = htmlspecialchars($user['pseudo']);
-        $solde = number_format($user['solde'], 2, ',', ' ');
-        $tableRows .= "
-            <tr>
-                <td><a href=\"index.php?page=profilboard&pseudo=$pseudo\">$photoProfil</a></td>
-                <td>$pseudo</td>
-                <td>$solde</td>
-            </tr>
-        ";
-    endforeach; ?>
+    <?php 
+foreach ($usersWithSolde as $user): 
+    $photoProfil = "<img src=\"{$user['image']}\" alt=\"Profil\" width=\"25\">";
+    $pseudo = htmlspecialchars($user['pseudo']);
+    $solde = number_format($user['solde'], 2, ',', ' ');
+    $lienProfil = "index.php?page=profilboard&pseudo=$pseudo";
+    $tableRows .= "
+        <tr onclick=\"window.location.href='{$lienProfil}';\" style=\"cursor:pointer;\">
+            <td>$photoProfil</td>
+            <td>$pseudo</td>
+            <td>$solde</td>
+        </tr>
+    ";
+endforeach; 
+?>
+
     
     <div class="table-responsive">
         <table class="leaderboard-table">
