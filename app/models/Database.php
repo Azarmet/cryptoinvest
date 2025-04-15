74fbcd00d3d6a0d7762ce218524117e43dada1aa
Roirand Lucas <roirand.lucas@outlook.fr>
2025-04-15 10:08:54 +0200

    modif securité database + logger

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,22 +1,18 @@
-<?php
-namespace App\Models;
+<?php namespace App\Models;
 
 use Dotenv\Dotenv;
 use PDO;
 use PDOException;
-
+use App\Utils\Logger;
 class Database
 {
-    // Instance unique de la classe
     private static $instance = null;
-    // Objet PDO de connexion
     private $pdo;
 
-    // Constructeur privé pour empêcher l'instanciation directe
     private function __construct()
     {
-        // Charge les variables d'environnement depuis le fichier .env
-        $dotenv = Dotenv::createImmutable(RACINE);  // RACINE définie dans index.php
+        // Chargement des variables d'environnement
+        $dotenv = Dotenv::createImmutable(RACINE);
         $dotenv->load();
 
         $host = $_ENV['DB_HOST'];
@@ -29,14 +25,14 @@ class Database
             $this->pdo = new PDO($dsn, $username, $password, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
             ]);
         } catch (PDOException $e) {
-            // En cas d'erreur, on arrête le script avec un message
-            die('Erreur de connexion à la base de données : ' . $e->getMessage());
+            Logger::log('Erreur PDO : ' . $e->getMessage());
+            die('Erreur de connexion à la base de données.'); // Message générique
         }
     }
 
-    // Méthode statique pour obtenir l'instance unique
     public static function getInstance()
     {
         if (self::$instance === null) {
@@ -45,10 +41,10 @@ class Database
         return self::$instance;
     }
 
-    // Retourne l'objet PDO de connexion
     public function getConnection()
     {
         return $this->pdo;
     }
 }
+
 ?>
